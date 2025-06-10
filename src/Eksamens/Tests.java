0e61e779413a34896ee1857641d649d25491c502
Denisenko <aominebas2006@gmail.com>
2025-06-10 23:55:23 +0300

    Izveidoju objektu

--- src/Eksamens/Tests.java
+++ src/Eksamens/Tests.java
@@ -1,9 +1,23 @@
 package Eksamens;
 
+import java.util.ArrayList;
+
 public class Tests {
+	static class Jautajums {
+        String teksts;
+        String[] atbildes;
+        int pareizaAtbilde;
+
+        Jautajums(String teksts, String[] atbildes, int pareizaAtbilde) {
+            this.teksts = teksts;
+            this.atbildes = atbildes;
+            this.pareizaAtbilde = pareizaAtbilde;
+        }
+    }
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		ArrayList<Jautajums> jautajumi = new ArrayList<Jautajums>();
+		
 
 	}
 
